1633e91495232b198704edef0cc5e467cb583ff2
ViniciusTei <viniciustprates@gmail.com>
2024-05-08 08:46:51 -0300

    feat made dashboard more responsive

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getSummary } from "@/services/firebase";
 import { useAuth } from "@/context/auth";
 import Icon from "./ui/icon";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 function Dashboard() {
   const { user } = useAuth();
@@ -16,20 +17,25 @@ function Dashboard() {
 
   return (
     <main className="w-full px-8 pt-8 flex-grow flex flex-col overflow-auto">
-      <section className="flex items-center justify-evenly gap-4 w-full">
-        <InsightCard title="Balance" value={data?.balance} />
-        <InsightCard title="Total expenses" value={data?.expense} />
-        <InsightCard title="Total income" value={data?.income} />
+      <section className="w-full">
+        <ScrollArea className="w-full">
+          <div className="flex w-full space-x-2">
+            <InsightCard title="Balance" value={data?.balance} />
+            <InsightCard title="Total expenses" value={data?.expense} />
+            <InsightCard title="Total income" value={data?.income} />
+            <ScrollBar orientation="horizontal" />
+          </div>
+        </ScrollArea>
       </section>
       <section className="mt-8 max-h-full flex-grow">
-        <div className="flex items-center justify-between">
+        <div className="flex justify-between flex-col md:items-center md:flex-row">
           <h2 className="text-xl font-semibold">Transactions</h2>
           <div className="flex items-center gap-4">
             <MonthPicker />
             <NewTransactions />
           </div>
+          <ExpensesTable />
         </div>
-        <ExpensesTable />
       </section>
     </main>
   );
