b830eb77f4e451c0d5bc331e98a487ee1386fd45
Vinicius Teixeira <vinicius.teixeira@cbrdoc.com.br>
2026-03-23 08:24:09 -0300

    fix: atualizar regras de linting para ignorar variáveis não utilizadas

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,14 +5,15 @@ import tseslint from "typescript-eslint"
 export default defineConfig([
 	{
 		files: ["./app/**/*.{ts,tsx}"],
-		ignores: ["./app/**/*.{test,spec}.{ts,tsx}", "./app/test/*"],
+		ignores: ["./app/**/*.{test,spec}.{ts,tsx}", "./app/test/*", "./app/domain/test/*"],
 		plugins: {
 			js,
 			tseslint,
 		},
 		extends: ["js/recommended", "tseslint/recommended"],
 		rules: {
-			"no-unused-vars": "warn",
+			"no-unused-vars": "off",
+    	"@typescript-eslint/no-unused-vars": ["warn", { "varsIgnorePattern": "^_", "argsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" }]
 		},
 	},
 ]);
